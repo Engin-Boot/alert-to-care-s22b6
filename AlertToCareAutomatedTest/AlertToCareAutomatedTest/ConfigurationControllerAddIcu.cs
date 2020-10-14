df46221ef68b83e6f15e1a2d17d3d010ccddfca3
PranshuSachan003 <pranshusachan65@gmail.com>
2020-10-14 14:21:35 +0530

    automation test done

--- AlertToCareAutomatedTest/AlertToCareAutomatedTest/ConfigurationControllerAddIcu.cs
+++ AlertToCareAutomatedTest/AlertToCareAutomatedTest/ConfigurationControllerAddIcu.cs
@@ -3,26 +3,29 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RestSharp;
 using Newtonsoft.Json;
 
-
-
 namespace AlertToCareAutomatedTest
 {
     [TestClass]
     public class ConfigurationControllerAddIcu
     {
         [TestMethod]
-        public bool ConfigureNewIcuTest()
+        public void ConfigurationControllerAddIcuTest()
         {
-            var restClient = new RestClient("http://localhost:54384/api/");
-            var restRequest = new RestRequest("Configuration/", Method.POST);
-            CaseStudy2.Model.IcuSetUpData icusetdata = new CaseStudy2.Model.IcuSetUpData();
-            //icusetdata.IcuId = 3;
-            icusetdata.BedsCount = 5;
-            icusetdata.Layout = "circle";
-            restRequest.AddJsonBody(JsonConvert.SerializeObject(icusetdata));
-            IRestResponse restResponse = restClient.Execute(restRequest);
-            return restResponse.IsSuccessful;
-        }
+            RestClient restClient = new RestClient("http://localhost:62527/api/");
 
+            //Creating request to get data from server
+            var restRequest = new RestRequest("Configuration/", Method.POST);
+            //  restRequest.AddUrlSegment("{id}", 1);
+            Model.IcuSetUpData icudata = new Model.IcuSetUpData();
+            icudata.BedsCount = 2;
+            icudata.Layout = "Circle";
+           
+            restRequest.AddJsonBody(JsonConvert.SerializeObject(icudata));
+
+            IRestResponse restResponse = restClient.Execute(restRequest);
+            // return restResponse.IsSuccessful;
+            Assert.AreEqual(true, restResponse.IsSuccessful);
+            //Console.WriteLine(restResponse.Content);
+        }
     }
 }
