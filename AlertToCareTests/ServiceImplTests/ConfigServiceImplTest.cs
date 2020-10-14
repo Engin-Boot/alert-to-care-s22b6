1afe69cb1ec403fe3d53c9020a49e3d445c75336
Chethan S V <Chethan.SV@philips.com>
2020-10-14 11:36:43 +0530

    Tests

---
+++ AlertToCareTests/ServiceImplTests/ConfigServiceImplTest.cs
@@ -0,0 +1,29 @@
+﻿using CaseStudy2.Model;
+using CaseStudy2.ServiceImpl;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AlertToCareTests.ServiceImplTests
+{
+    public class ConfigServiceImplTest
+    {
+        /* IcuSetUpData _icuSetUpData = new IcuSetUpData();
+        ConfigurationImpl configurationImpl = new ConfigurationImpl();
+        public ConfigServiceImplTest()
+        {
+            _icuSetUpData.IcuId = 1;
+            _icuSetUpData.BedsCount = 20;
+            _icuSetUpData.Layout = "Cr";
+        }
+
+       [Fact]
+        public void AddIcuTest()
+        {
+
+            var result = configurationImpl.AddNewIcu(_icuSetUpData);
+            Assert.IsType<bool>(result);
+        }*/
+    }
+}
---
+++ AlertToCareTests/ServiceImplTests/OccupancyServiceImplTest.cs
@@ -0,0 +1,51 @@
+﻿using CaseStudy2.Service;
+using CaseStudy2.ServiceImpl;
+using CaseStudy2.Model;
+using Moq;
+using Xunit;
+
+namespace AlertToCareTests.ServiceImplTests
+{
+    public class OccupancyServiceImplTest
+    {
+        /*private Mock<IOccupancyService> _mockRepo;
+
+        private OccupancyServiceImpl occupancyServiceImpl=new OccupancyServiceImpl();
+        PatientData patientData = new PatientData();
+
+        public OccupancyServiceImplTest()
+        {
+            _mockRepo = new Mock<IOccupancyService>();
+            // occupancyServiceImpl = new OccupancyServiceImpl(_mockRepo.Object);
+            patientData.Id = 1;
+            patientData.Name = "Cr";
+            patientData.Address = "UP";
+            patientData.Email = "Cr@gmail.com";
+            patientData.RespRate = 90.0;
+            patientData.Spo2 = 80.0;
+            patientData.Bpm = 100.0;
+            patientData.IcuId = 10;
+            patientData.BedId = "B1";
+        }
+       // [Fact]
+        public void CheckBedStatusTest()
+        {
+            string s = "bedId";
+            var result = occupancyServiceImpl.CheckBedStatus(s);
+            Assert.IsType<bool>(result);
+        }
+        [Fact]
+        public void DischargePatientTest()
+        {
+            var result = occupancyServiceImpl.DishchargePatient(1);
+            Assert.IsType<bool>(result);
+        }
+      /*  [Fact]
+        public void AddPatientTest()
+        {
+
+            var result = occupancyServiceImpl.AddNewPatient(patientData);
+            Assert.IsType<bool>(result);
+        }*/
+    }
+}
